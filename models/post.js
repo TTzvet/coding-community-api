fa89ba30a723d01b54be2d5d92116ca8a9853a13
Teodora Tzvetkova <tt18aav@herts.ac.uk>
2020-12-23 14:27:06 +0000

    refactored-post-create-method-as-validation-handled-separately

--- models/post.js
+++ models/post.js
@@ -3,15 +3,11 @@ const mongoose = require ('mongoose')
 const postSchema = new mongoose.Schema({
     title: {
         type: String,
-        required: 'Title required',
-        minlength: 5,
-        maxlength: 150
+        required: true
     },
     body: {
         type: String,
-        required: 'Body required',
-        minlength: 15,
-        maxlength: 1500
+        required: true
     }
 });
 
